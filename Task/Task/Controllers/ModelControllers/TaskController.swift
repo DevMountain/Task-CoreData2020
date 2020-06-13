280a0b917ed6a13ba434208b0cf1bc0af5379925
Connor Holland <connorholland@Connors-MacBook-Pro.local>
2020-06-13 13:45:38 -0600

    Task

---
+++ Task/Task/Controllers/ModelControllers/TaskController.swift
@@ -0,0 +1,9 @@
+//
+//  TaskController.swift
+//  Task
+//
+//  Created by Connor Holland on 6/11/20.
+//  Copyright © 2020 Karl Pfister. All rights reserved.
+//
+
+import Foundation
---
+++ Task/Task/Controllers/ViewControllers/ButtonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ButtonTableViewCell.swift
+//  Task
+//
+//  Created by Connor Holland on 6/11/20.
+//  Copyright © 2020 Karl Pfister. All rights reserved.
+//
+
+import UIKit
+
+class ButtonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
